860e2aac4679a02174356eac217a2a4a5927d7ec
Sean Park <jungbeanshaanprk@gmail.com>
2024-11-28 16:20:14 -0800

    Added concurrent client support

--- src/Server.cpp
+++ src/Server.cpp
@@ -14,7 +14,7 @@ void handle_client(int client_fd) {
   char buffer[1024] = {0};
   while (true) {
     int test = recv(client_fd, buffer, sizeof(buffer), 0);
-    std::cout << test;
+    std::cout << "Input: " << test;
     if (test == 1) {
       break;
     }
