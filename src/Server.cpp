1922556cf65c617d707267c05cb0bf807281c433
Sean Park <jungbeanshaanprk@gmail.com>
2024-11-29 16:12:13 -0800

    Handshake Step 1

--- src/Server.cpp
+++ src/Server.cpp
@@ -305,8 +305,10 @@ int main(int argc, char **argv) {
             is_master = false;
             std::string host_and_port = argv[++i];
             master_host = host_and_port.substr(0, host_and_port.find(" "));
+            master_port = host_and_port.substr(host_and_port.find(" "), host_and_port.size());
             std::cout << "host_and_port: " << host_and_port << std::endl;
             std::cout << "master_host: " << master_host << std::endl;
+            std::cout << "master_port: " << master_port << std::endl;
             // master_port = std::stoi(argv[++i]);
         }
     }
