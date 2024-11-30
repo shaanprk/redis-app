1c8d5469cfd312ca56509e077ad5b59c4bf2ae72
Sean Park <jungbeanshaanprk@gmail.com>
2024-11-29 16:07:12 -0800

    Handshake Step 1

--- src/Server.cpp
+++ src/Server.cpp
@@ -303,7 +303,7 @@ int main(int argc, char **argv) {
             std::cout << "port: " << port << std::endl;
         } else if (strcmp(argv[i], "--replicaof") == 0) {
             is_master = false;
-            master_host = argv[++i];
+            master_host = strtok(argv[++i], " ");
             std::cout << "master_host: " << master_host << std::endl;
             // master_port = std::stoi(argv[++i]);
         }
